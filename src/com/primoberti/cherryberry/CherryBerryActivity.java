8ab851dc96ad8d30ee6ad70f3046a237b29f8984
Alberto Salmerón Moreno <alberto.salmeron@gmail.com>
2012-02-04 14:56:17 +0100

    Added GPLv3 license file, copyright header

--- src/com/primoberti/cherryberry/CherryBerryActivity.java
+++ src/com/primoberti/cherryberry/CherryBerryActivity.java
@@ -1,13 +1,33 @@
+/*
+ * Copyright 2012 Alberto Salmerón Moreno
+ * 
+ * This file is part of CherryBerry - https://github.com/berti/CherryBerry.
+ * 
+ * CherryBerry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CherryBerry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CherryBerry.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.primoberti.cherryberry;
 
 import android.app.Activity;
 import android.os.Bundle;
 
 public class CherryBerryActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
